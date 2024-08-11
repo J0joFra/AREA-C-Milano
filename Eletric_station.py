321c657bf79a92f3867e39c23d7c2e23fad82502
Joaquim Francalanci <jofrancalanci@gmail.com>
2024-08-11 16:17:51 +0200

    Importazione Dataset

--- Eletric_station.py
+++ Eletric_station.py
@@ -1,3 +1,8 @@
 #Importazioni Librerie
 import pandas as pd
 
+#Importazione Dataset
+file_path = r"C:\Users\JoaquimFrancalanci\OneDrive - ITS Angelo Rizzoli\Desktop\Progetti\Youtube\Ogury\ElectricityCar.xlsx"
+df = pd.read_excel(file_path)
+
+#Visualizzazione Dataset
